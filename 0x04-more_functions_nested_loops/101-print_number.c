d1789c3fd74ab986054b09c5f88b94442cc6753f
XOLANIRH <xolanirh@gmail.com>
2022-10-20 16:48:08 -0700

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_number - Function
+ * @n: int number
+ * Description: can ony use char
+ */
+
+void print_number(int n)
+{
+	long m;
+	int c;
+	long num;
+
+	num = n;
+
+	if (num < 0)
+	{
+		num *= -1;
+		_putchar('-');
+	}
+	m = 1;
+	c = 1;
+	while (c)
+	{
+		if (num / (m * 10) > 0)
+			m *= 10;
+		else
+			c = 0;
+	}
+
+	while (num >= 0)
+	{
+		if (m == 0)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / m % 10) + '0');
+			m /= 10;
+		}
+	}
+}
