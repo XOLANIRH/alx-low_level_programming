b149b951a6e94fe55743825429dec339b8061847
XOLANIRH <xolanirh@gmail.com>
2022-10-19 18:11:16 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print _alphabet - prints the alphabet in lower cases
+ */
+void print_alphabet(void)
+{
+	cha la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
