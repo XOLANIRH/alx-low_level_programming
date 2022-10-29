501a303210206febb9f1e44ed20f76c23ac1228c
XOLANIRH <xolanirh@gmail.com>
2022-10-29 15:47:19 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * Add one line to this code, so that the program prints, followed by a new line.
- * description - you are not allowed to use a, you are not allowed t
- * only one statement
+ * main: Entry point
+ *
+ * description: magic code
+ *
+ * Return: ALways 0
  */
 
 int main(void)
@@ -14,6 +17,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
 	/*
 	 * write your line of code here...
 	 * Remember:
@@ -22,9 +26,8 @@ int main(void)
 	 *   - only one statement
 	 *   - you are not allowed to code anything else than this line of code
 	 */
-	*(p+5) = 98;
-	/*...so that this prints 98\n */
-
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
